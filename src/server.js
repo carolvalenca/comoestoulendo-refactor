b613f3831fe70f86883765e5d0b3b460471ad549
Caroliny Valença <caroliny.leandro@ccc.ufcg.edu.br>
2021-01-22 19:14:31 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,3 @@
-const express = require('express');
-const routes = require('./routes');
-// const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-
-// const app = express();
-
-// dotenv.config();
-
-// mongoose.connect(process.env.DB_URL,
-//     { useNewUrlParser: true },
-//     { useUnifiedTopology: true });
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(routes);
-
-// app.listen(8080);
-
 const app = require('./app')
 
 app.listen(process.env.PORT || 8080)
